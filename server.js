35a9457ccbdcf8c549e05cbba2e58a8b6f973c92
purv04 <puru.chaturvedi04@gmail.com>
2025-04-23 20:40:03 +0530

    updated port

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.post('/api/todos', (req, res) => {
   }
 });
 
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port 8080`));
